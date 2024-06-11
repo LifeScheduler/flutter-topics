08fee2be6d3fe1e12ba43ae83a9ec476919b7a0d
Artur Assis Alves <arturassiscomp@gmail.com>
2024-06-10 21:17:27 -0300

    fix incorrect

--- lib/generic_topics/stateless_and_statefull_widgets/counter_incorreto.dart
+++ lib/generic_topics/stateless_and_statefull_widgets/counter_incorreto.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
+void main() => runApp(MaterialApp(
       home: Scaffold(body: Center(child: Counter())),
     ));
 
-class Counter extends StatefulWidget {
-  const Counter({super.key});
+class Counter extends StatelessWidget {
+  Counter({super.key});
 
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: () => setState(() {
-              counter++;
-              debugPrint(counter.toString());
-            }),
+        onPressed: () {
+          counter++;
+          debugPrint(counter.toString());
+        },
         child: Text('$counter'));
   }
 }
